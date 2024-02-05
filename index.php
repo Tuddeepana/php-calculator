17ea85cbc8e722ce7af48fdf1d3df2eb02b0cf5e
Tuddeepana <tauddeepana@gmail.com>
2024-02-05 12:20:12 +0530

    simple cal

--- index.php
+++ index.php
@@ -7,6 +7,23 @@
 </head>
 <body>
 
+<form action='' method="GET">
+    <input type="text" name="num1" placeholder= "Number 1"><br><br>
+    <input type="text" name="num2" placeholder= "Number 2"><br><br>
+
+
+
+    <select name="operator">
+    <option value="none">None</option>
+    <option value="add">Add</option>
+    <option value="sub">Subtraction</option>
+    <option value="divide">Divide</option>
+</select><br><br>
+
+
+<button type='submit' name='submit'>Calculate</button>
+
+</form>
 
 
 
@@ -14,6 +31,43 @@
 
 
 <?php
+
+    if(isset($_GET['submit'])){
+        $num1 = $_GET['num1'];
+        $num2 = $_GET['num2'];
+
+        $operator = $_GET['operator'];
+
+
+        //Display answer according the operator
+        switch ($operator) {
+            case "add":
+                $adding = $num1 + $num2;
+                echo "Answer is :" .($num1 + $num2);
+                break;
+            
+            case "sub":
+                $subtract = $num1-$num2;
+                echo "Answer is :" .($num1 - $num2);
+                break;
+
+
+
+            case "divide":
+                $divide = $num1 / $num2;
+                echo "Answer is :" .($num1 / $num2);
+                break;
+
+                default:
+                echo  "Please set the operator";
+            
+        }
+        
+
+
+
+    }
+
 
 
 
